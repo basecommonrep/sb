6bf3057d1f12043af529d7a7010062426818ea53
Alex Drobazko <drobazko@gmail.com>
2013-07-18 17:30:45 +0300

    added a default image rendering

--- pp_image.php
+++ pp_image.php
@@ -1,4 +1,26 @@
 <?
+
+function sample_image() {
+	header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); 
+	header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+	header("Cache-Control: no-cache, must-revalidate"); 
+	header("Cache-Control: post-check=0,pre-check=0"); 
+	header("Cache-Control: max-age=0"); 
+	header("Pragma: no-cache");
+	
+	header('Accept-Ranges: bytes');
+	header('Connection: close');
+	header('Content-Type: image/gif');
+	header("Content-Disposition: inline; filename=im");
+
+	$c = curl_init();
+	curl_setopt($c, CURLOPT_URL, 'http://placehold.it/60x60/C5D9EB&text=no+image');
+	curl_setopt($c, CURLOPT_PROXY, '10.10.16.8:3128');
+	curl_setopt($c,CURLOPT_BINARYTRANSFER, true);
+	$result = @curl_exec($c);
+	curl_close($c); 
+}
+
 include('incs/config.inc');
 include('incs/mssql.class.inc');
 
@@ -20,6 +42,8 @@ $query = <<<QUERY
 	where c.i_210 = '{$_GET['app_number']}'
 	order by c2.idvariant
 QUERY;
+
+
 
 if ( (int)$mssql->executeQuery($query) != -1 ) {
 	$data = $mssql->getRow('num');
@@ -45,9 +69,10 @@ if ( (int)$mssql->executeQuery($query) != -1 ) {
 		readfile($f);
 	}
 	else
-		print('file not exists');
-
+		sample_image();
 }
+else
+	sample_image();
 
 // =================
 // відключення від БД
