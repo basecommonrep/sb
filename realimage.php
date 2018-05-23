0c76d59b337570873f47327400e1e9680599192a
Ganenko M <ganenkomikhail@gmail.com>
2018-05-23 16:59:41 +0300

    added images displaying to GEO full biblio

--- realimage.php
+++ realimage.php
@@ -6,6 +6,7 @@ $idVariant = (int)$_GET['idVariant'];
 $idClaim = (int)$_GET['idClaim'];
 $varIndex = (int)$_GET['varIndex'];
 $imageType = $_GET['it'];
+$geo_img_id = $_GET['id']; // geo image id
 
 if ( $dbn == 'pp' )
 	$f = PPImageTemplatePath.'pp_'.$idVariant.'-'.$varIndex.'.jpg';
@@ -15,6 +16,8 @@ elseif ( $dbn == 'madrid' || $dbn == 'madridall' )
 	$f = 'tempdocs/madrid_'.$idClaim.'.'.$imageType;
 elseif ( $dbn == 'wkm' )
 	$f = 'tempdocs/wkm_'.$idClaim.'.jpg';
+elseif (substr($dbn, 0, 3) == 'geo')
+  $dbn = 'geo';
 
 if ( file_exists($f) ) 
 {
@@ -35,8 +38,43 @@ if ( file_exists($f) )
 	
 	readfile($f);
 }
+elseif ( $dbn == 'geo' && !empty($geo_img_id) )
+{
+  $connection = $GLOBALS['connection'][$dbn];
+  $link = mssql_connect($connection['host'], $connection['login'], $connection['pass']);
+  mssql_select_db($connection['dbname'], $link);
+  $query = mssql_query("SELECT [image] FROM [dbo].[geo_image] WHERE id=$geo_img_id");  
+  $row = mssql_fetch_assoc($query);
+  mssql_close($link);
+  
+  if(!empty($row['image']))
+    $img = imagecreatefromstring($row['image']);
+  else
+    die('file not exists');
+  
+  if ( @array_pop(unpack('H4', $row['image'])) == 'ffd8' ) 
+  {
+    header("Content-type: image/jpeg");
+    header("Content-Disposition: inline; filename=$geo_img_id.jpg");
+    imagejpeg($img);
+    imagedestroy($img);
+  } 
+  elseif ( @array_pop(unpack('H16', $row['image'])) == '89504e470d0a1a0a' ) 
+  {
+    header("Content-type: image/png");
+    header("Content-Disposition: inline; filename=$geo_img_id.png");
+    imagepng($img);
+    imagedestroy($img);
+  } 
+  else
+  {
+    header("Content-type: image/jpeg");
+    header("Content-Disposition: inline; filename=$geo_img_id.jpg");
+    imagejpeg($img);
+    imagedestroy($img);
+  }     
+}
 else
 	print('file not exists');
-
 
 ?>
